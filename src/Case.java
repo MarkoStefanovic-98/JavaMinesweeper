0896b8ea1cf50b248647b65b4a6269f6c20cce15
MarkoStefanovic-98 <benoit.valleea@hotmail.fr>
2019-01-11 00:28:33 +0100

    The final

--- src/Case.java
+++ src/Case.java
@@ -2,17 +2,15 @@
 public class Case {
 	
 	//Private
-	boolean bombe, decouverte, drapeau;
+	boolean bombe, decouverte;
 	int valeur;
 	//Private fin
 	
 	public boolean isBombe()		{ return bombe; }
 	public boolean isDecouverte() 	{ return decouverte; }
-	public boolean isDrapeau()		{ return drapeau; }
 	public int getValeur()			{ return valeur; }
 	public void setBombe(boolean b)		{ bombe = b; }
 	public void setDecouverte(boolean d){ decouverte = d; }
-	public void setDrapeau(boolean d)	{ drapeau = d; }
 	public void setValeur(int v)		{ valeur = v;}
 }
 //Test PUSH BENOIT
--- src/Tableau.java
+++ src/Tableau.java
@@ -3,9 +3,7 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Tableau {
-	enum Difficulte {Facile, Moyen, Difficile};
 	int nombreCase, nombreMine, caseChoisie, nombreCoup, nbCaseDecouverte, nbChoixRestant;
-	Difficulte difficulte;
 	Scanner clavier = new Scanner(System.in);
 	Case tableauCases[][];
 	boolean perdu = false;
@@ -15,8 +13,13 @@ public class Tableau {
 		boolean perdu = false, gagne = false;
 		do
 		{
-			nbCaseDecouverte = 0;
-			for(int x=0; x<nombreCase; x++) for(int y=0; y<nombreCase; y++) if(tableauCases[x][y].isDecouverte()) nbCaseDecouverte++; 
+			nbCaseDecouverte = 0; 
+			// on parcour la matrice pour compter le nombre de cases découvertes
+			for(int x=0; x<nombreCase; x++){
+				for(int y=0; y<nombreCase; y++){
+					if(tableauCases[x][y].isDecouverte()) nbCaseDecouverte++; 
+				}
+			}
 			nbChoixRestant = nombreCase*nombreCase - nbCaseDecouverte - nombreMine; 
 			if(nbChoixRestant==0) gagne = true;
 			System.out.println("\nTABLEAU | " + nombreMine + " mines | "+(nbChoixRestant)+" choix restants | "+nombreCoup+" coups effectués");
@@ -82,8 +85,6 @@ public class Tableau {
 					if(tableauCases[X+a][Y+b].getValeur() == 0)
 					{
 						tableauCases[X+a][Y+b].setDecouverte(true);
-						tableauCases[X][Y].setDrapeau(true);	
-						if(!tableauCases[X+a][Y+b].isDrapeau()) // pour détruire le StackOverflowError
 							propagationZero(X+a, Y+b);
 						for(int A=-1; A<2; A++)
 							for(int B=-1; B<2; B++)
@@ -94,18 +95,30 @@ public class Tableau {
 			}
 	}
 	
-	void reglageDifficulte(Difficulte diff) {
-		switch(diff)
+	void reglageDifficulte() {
+
+		int difficulte = 0;
+		System.out.println("\nChoissisez la difficulté :\n(1) Facile \n(2) Moyen \n(3) Difficile\n");
+		try { difficulte = clavier.nextInt();  } // si l'utilisateur n'entre pas un nombre
+		catch (InputMismatchException e) { System.out.println("ERREUR : Entrez un nombre !"); }
+
+		switch(difficulte)
 		{
-			case Facile	: nombreCase = 10; nombreMine = 10; break;
-			case Moyen	: nombreCase = 15; nombreMine = 20; break;
-			case Difficile	: nombreCase = 20; nombreMine = 30; break;
+		case 1:
+			nombreCase = 10; nombreMine = 10; break;
+		case 2:
+			nombreCase = 15; nombreMine = 20; break;
+		case 3:
+			nombreCase = 20; nombreMine = 50; break;
+		default:
+			nombreCase = 15; nombreMine = 20; break;
 		}
-		
-		this.difficulte = diff;
 	}
 	
 	void initialisation() {
+		
+		reglageDifficulte();
+		
 		int i=0, x=0, y=0, a=0, b=0, z=0;
 		tableauCases = new Case[nombreCase][nombreCase]; // Initialisation d'une matrice pour localiser les cases
 		
@@ -138,10 +151,9 @@ public class Tableau {
 	}
 	 
 	public static void main(String[] args) {
-		System.out.println("\tMineSweeper");
+		System.out.println("\tMineSweeper | Si tu perds... TU MEURS !");
 		
 		Tableau grille = new Tableau();
-		grille.reglageDifficulte(Difficulte.Facile);
 		grille.initialisation();
 		grille.afficher();
 	}
