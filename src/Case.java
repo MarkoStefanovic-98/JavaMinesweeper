ff60c92c1b07e60feb92e8d2363e46642cecfeb0
MarkoStefanovic-98 <markostef@hotmail.fr>
2019-01-11 11:37:24 +0100

    Le last one

--- src/Case.java
+++ src/Case.java
@@ -9,9 +9,11 @@ public class Case {
 	
 	public boolean isBombe()		{ return bombe; }
 	public boolean isDecouverte() 	{ return decouverte; }
+	public boolean isDrapeau()		{ return drapeau; }
 	public int getValeur()			{ return valeur; }
 	public void setBombe(boolean b)		{ bombe = b; }
 	public void setDecouverte(boolean d){ decouverte = d; }
+	public void setDrapeau(boolean d)	{ drapeau = d; }
 	public void setValeur(int v)		{ valeur = v;}
 }
 //Test PUSH BENOIT
